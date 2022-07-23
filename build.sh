dee47033f341a98b1deba6b9e9ece7a1394454ef
Nick <nift4@protonmail.com>
2022-07-23 15:17:31 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -56,8 +56,8 @@ if [ "${generate_incremental}" == "true" ]; then
     fi
     cp "${new_target_files_path}" "${ROM_DIR}"
 fi
-if [ -e "${outdir}"/*$(date +%Y)*.zip ]; then
-    export finalzip_path=$(ls "${outdir}"/*$(date +%Y)*.zip | tail -n -1)
+if [ -e "${outdir}"/droid-ng-*.zip ]; then
+    export finalzip_path=$(ls "${outdir}"/droid-ng-*.zip | tail -n -1)
 else
     export finalzip_path=$(ls "${outdir}"/*"${device}"-ota-*.zip | tail -n -1)
 fi
